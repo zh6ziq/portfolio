96725852ba1349cf57e6fb43cf13fa966c529e96
zh6ziq <71532216+zh6ziq@users.noreply.github.com>
2020-10-20 06:42:49 +0800

    portfolio
    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//require express
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+
+//serving static files
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+
+//callback home.html page
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
